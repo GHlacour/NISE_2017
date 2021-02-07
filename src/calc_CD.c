a25dda361636b33bf2b5b4a542c3faa2fa55d51c
GHlacour <t.l.c.jansen@rug.nl>
2021-02-07 12:05:51 +0100

    Pollished CD calculation

--- src/calc_CD.c
+++ src/calc_CD.c
@@ -114,8 +114,6 @@ void calc_CD(t_non *non){
 
   vecr=(float *)calloc(non->singles,sizeof(float));	
   veci=(float *)calloc(non->singles,sizeof(float));
-//  vecr_old=(float *)calloc(non->singles,sizeof(float));
-//  veci_old=(float *)calloc(non->singles,sizeof(float));
   mu_eg=(float *)calloc(non->singles,sizeof(float));
   pos=(float *)calloc(non->singles,sizeof(float));
 
@@ -138,95 +136,94 @@ void calc_CD(t_non *non){
     }
     if (non->cluster==-1 || non->cluster==cl){
 
-    // Loop over initial sites
-    for (j=0;j<N;j++){ 
+      // Loop over initial sites
+      for (j=0;j<N;j++){ 
 
-    // Loop over polarizations of the initial excitation      
-    for (x=0;x<3;x++){
-      // Read mu(ti)
-      if (read_mue(non,mu_eg,mu_traj,ti,x)!=1){
-	printf("Dipole trajectory file to short, could not fill buffer!!!\n");
-	printf("ITIME %d %d\n",ti,x);
-	exit(1);
-      }
-      // Initialize excitation on initial site
-      clearvec(vecr,non->singles);
-      clearvec(veci,non->singles);
-      vecr[j]=mu_eg[j];
-//      copyvec(vecr,vecr_old,non->singles);
-//      copyvec(vecr,mu_eg,non->singles);
-      // Loop over delay
-      for (t1=0;t1<non->tmax;t1++){
-	tj=ti+t1;
-	// Read Hamiltonian
-	if (read_He(non,Hamil_i_e,H_traj,tj)!=1){
-	  printf("Hamiltonian trajectory file to short, could not fill buffer!!!\n");
-	  exit(1);
-	}
+        // Loop over polarizations of the initial excitation      
+        for (x=0;x<3;x++){
+          // Read mu(ti)
+          if (read_mue(non,mu_eg,mu_traj,ti,x)!=1){
+       	    printf("Dipole trajectory file to short, could not fill buffer!!!\n");
+	    printf("ITIME %d %d\n",ti,x);
+	    exit(1);
+          }
+          // Initialize excitation on initial site
+          clearvec(vecr,non->singles);
+          clearvec(veci,non->singles);
+          vecr[j]=mu_eg[j];
 
-        // Loop over polarization values y for mu
-        for (y=0;y<3;y++){
-          // Exclude values taken up by the first interaction
-          if (y!=x){	
-            // Find corresponding value for the polarization used for the distance matrix
-            z=3-x-y;
-	    // Read mu(tj)
-	    if (read_mue(non,mu_eg,mu_traj,tj,y)!=1){
-	      printf("Dipole trajectory file to short, could not fill buffer!!!\n");
-	      printf("JTIME %d %d\n",tj,y);
+          // Loop over delay
+          for (t1=0;t1<non->tmax;t1++){
+	    tj=ti+t1;
+	    // Read Hamiltonian
+	    if (read_He(non,Hamil_i_e,H_traj,tj)!=1){
+	      printf("Hamiltonian trajectory file to short, could not fill buffer!!!\n");
 	      exit(1);
-            }
-            // Read positions
-            if (read_mue(non,pos,pos_traj,tj,z)!=1){
-              printf("Position trajectory file to short, could not fill buffer!!!\n");
-              printf("JTIME %d %d\n",tj,z);
-              exit(1);
-            }
-            posj=pos[j];
-	    // Do projection on selected sites if asked
-	    if (non->Npsites>0){
-	      projection(mu_eg,non);
 	    }
+
+            // Loop over polarization values y for mu
+            for (y=0;y<3;y++){
+              // Exclude values taken up by the first interaction
+              if (y!=x){	
+                // Find corresponding value for the polarization used for the distance matrix
+                z=3-x-y;
+	        // Read mu(tj)
+	        if (read_mue(non,mu_eg,mu_traj,tj,y)!=1){
+	          printf("Dipole trajectory file to short, could not fill buffer!!!\n");
+	          printf("JTIME %d %d\n",tj,y);
+	          exit(1);
+                }
+                // Read positions
+                if (read_mue(non,pos,pos_traj,tj,z)!=1){
+                  printf("Position trajectory file to short, could not fill buffer!!!\n");
+                  printf("JTIME %d %d\n",tj,z);
+                  exit(1);
+                }
+                posj=pos[j];
+	        // Do projection on selected sites if asked
+	        if (non->Npsites>0){
+	          projection(mu_eg,non);
+	        }
 	
-            sign=0;
-            // Determine the sign
-            if (z==0 & y==1 & x==2){sign=1;}//{sign=1;}
-            if (z==0 & y==2 & x==1){sign=-1;}//{}sign=-1;}
-            if (z==1 & y==0 & x==2){sign=-1;}//{sign=-1;}
-            if (z==2 & y==0 & x==1){sign=1;}//{sign=1;}
-            if (z==2 & y==1 & x==0){sign=-1;}//{sign=-1;}
-            if (z==1 & y==2 & x==0){sign=1;}//{sign=1;}
-            if (sign==0){
-              printf("Bug in CD routine.\n");
-              exit(1);
+                sign=0;
+                // Determine the sign
+                if (z==0 & y==1 & x==2){sign=1;}//{sign=1;}
+                if (z==0 & y==2 & x==1){sign=-1;}//{}sign=-1;}
+                if (z==1 & y==0 & x==2){sign=-1;}//{sign=-1;}
+                if (z==2 & y==0 & x==1){sign=1;}//{sign=1;}
+                if (z==2 & y==1 & x==0){sign=-1;}//{sign=-1;}
+                if (z==1 & y==2 & x==0){sign=1;}//{sign=1;}
+                if (sign==0){
+                  printf("Bug in CD routine.\n");
+                  exit(1);
+                }
+
+	        // Find response
+	        calc_CD1(re_S_1,im_S_1,t1,non,vecr,veci,mu_eg,pos,sign,posj);
+	      }
             }
 
-	    // Find response
-	    calc_CD1(re_S_1,im_S_1,t1,non,vecr,veci,mu_eg,pos,sign,posj);
-	  }
-        }
-
-	// Propagate vector
-	if (non->propagation==1) propagate_vec_coupling_S(non,Hamil_i_e,vecr,veci,non->ts,1);
-	if (non->propagation==0){
-	  if (non->thres==0 || non->thres>1){
-	    propagate_vec_DIA(non,Hamil_i_e,vecr,veci,1);
-	  } else {
-	    elements=propagate_vec_DIA_S(non,Hamil_i_e,vecr,veci,1);
-	    if (samples==non->begin){
-	      if (t1==0){
-		if (x==0){
-		  printf("Sparce matrix efficiency: %f pct.\n",(1-(1.0*elements/(non->singles*non->singles)))*100);
-		  printf("Pressent tuncation %f.\n",non->thres/((non->deltat*icm2ifs*twoPi/non->ts)*(non->deltat*icm2ifs*twoPi/non->ts)));
-		  printf("Suggested truncation %f.\n",0.001);
-		}
+	    // Propagate vector
+	    if (non->propagation==1) propagate_vec_coupling_S(non,Hamil_i_e,vecr,veci,non->ts,1);
+	    if (non->propagation==0){
+	      if (non->thres==0 || non->thres>1){
+	        propagate_vec_DIA(non,Hamil_i_e,vecr,veci,1);
+	      } else {
+	        elements=propagate_vec_DIA_S(non,Hamil_i_e,vecr,veci,1);
+	        if (samples==non->begin){
+	          if (t1==0){
+		    if (x==0){
+		      printf("Sparce matrix efficiency: %f pct.\n",(1-(1.0*elements/(non->singles*non->singles)))*100);
+		      printf("Pressent tuncation %f.\n",non->thres/((non->deltat*icm2ifs*twoPi/non->ts)*(non->deltat*icm2ifs*twoPi/non->ts)));
+		      printf("Suggested truncation %f.\n",0.001);
+		    }
+	          }
+	        }
 	      }
 	    }
-	  }
-	}
+          }
+        }
       }
-    }
-    }
     } // Cluster loop
   
     // Log time
