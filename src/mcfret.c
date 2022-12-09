1b46a7534f316f4c6bd79f648659c70f69b39178
V.Eric@rug.nl <p290445@pg-interactive.hpc.rug.nl>
2022-12-09 14:56:44 +0100

    #1 mcfret-Absorption light progress

--- src/mcfret.c
+++ src/mcfret.c
@@ -13,15 +13,44 @@
 /* Distribute the MCFRET calculations */
 void mcfret(t_non *non){
     /* Define variables and arrays */
-    /*Number of molecules*/
-    int nn2;
+   
     /* Response functions for emission and absorption: real and imaginary part*/
     float *re_S_Abs,*im_S_Abs;
     float *re_S_Emi,*im_S_Emi;
-    /*Hamiltonian of the whole system */
+    /*Vjesno: Question: Compared to DOS.c script we do not need veci and vecr because we do not want to project on the states?!*/
+    /*Hamiltonian of the whole system - all donors and acceptors included*/
     float *Hamil_i_e;
 
+/* Floats */
+    float shift1;
+  /* 1D Fourier transform */
+    fftw_complex *fftIn,*fftOut;
+    /*was is das?*/
+    fftw_plan fftPlan;
 
+/*File handles: Do we need the same ones like in DOS.c*/
+
+    FILE *H_traj,*mu_traj;
+    FILE *C_traj;
+    FILE *outone,*log;
+    FILE *Cfile;
+  /* Integers */
+    int nn2;
+    int itime,N_samples;
+    int samples;
+    int x,ti,tj,i,j;
+    int t1,fft;
+    int elements;
+    int cl,Ncl;
+
+  /* Time parameters */
+    time_t time_now,time_old,time_0;
+  /* Initialize time */
+    time(&time_now);
+    time(&time_0);
+    shift1=(non->max1+non->min1)/2;
+    printf("Frequency shift %f.\n",shift1);
+    non->shifte=shift1;
     /*Allocate memory for all the variables*/
     /*Allocate memory for the response functions*/
     nn2=non->singles*(non->singles+1)/2;
@@ -32,6 +61,55 @@ void mcfret(t_non *non){
     /*Allocate memory for the Hamiltonian matrix*/
     Hamil_i_e=(float *)calloc(nn2,sizeof(float));
 
+     /* Open Trajectory files */
+    H_traj=fopen(non->energyFName,"rb");
+    if (H_traj==NULL){
+        printf("Hamiltonian file not found!\n");
+        exit(1);
+    }
+      /*Here we want to call the routine for checking the trajectory files*/ 
+    control(non);
+
+    itime=0;
+  /* Do calculation*/
+    N_samples=(non->length-non->tmax1-1)/non->sample+1;
+    if (N_samples>0) {
+        printf("Making %d samples!\n",N_samples);
+    } else {
+      printf("Insufficient data to calculate spectrum.\n");
+      printf("Please, lower max times or provide longer\n");
+      printf("trajectory.\n");
+      exit(1);
+    }
+
+    if (non->end==0) non->end=N_samples;
+    if (non->end>N_samples){
+      printf(RED "Endpoint larger than number of samples was specified.\n" RESET);
+      printf(RED "Endpoint was %d but cannot be larger than %d.\n" RESET,non->end,N_samples);
+      exit(0);
+    }
+
+    log=fopen("NISE.log","a");
+    fprintf(log,"Begin sample: %d, End sample: %d.\n",non->begin,non->end);
+    fclose(log);
+
+    /* Read coupling */
+    /*Question: Vjesno - Can we already do something here to nullify certain couplings??*/
+    if (!strcmp(non->hamiltonian,"Coupling")){
+      C_traj=fopen(non->couplingFName,"rb");
+      if (C_traj==NULL){
+        printf("Coupling file not found!\n");
+        exit(1);
+      }
+      if (read_He(non,Hamil_i_e,C_traj,-1)!=1){
+        printf("Coupling trajectory file to short, could not fill buffer!!!\n");
+        exit(1);
+      }
+      fclose(C_traj);
+  }
+
+
+  
     /* Call the MCFRET Routine */
     if (!strcmp(non->technique, "MCFRET") || (!strcmp(non->technique, "MCFRET-Autodetect")) || (!strcmp(non->technique, "MCFRET-Absorption"))
         || (!strcmp(non->technique, "MCFRET-Emission")) || (!strcmp(non->technique, "MCFRET-Coupling")) || (!strcmp(non->technique,
