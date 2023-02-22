0abee38dc72536adf7ae2270e6946a6c78839561
GHlacour <t.l.c.jansen@rug.nl>
2023-02-22 18:00:16 +0100

    Fixed indentation issue in absorption.c

--- src/absorption.c
+++ src/absorption.c
@@ -98,13 +98,13 @@ void absorption(t_non *non){
     /* If clusters are selectred open cluster information file */
     if (non->cluster!=-1){
       if (read_cluster(non,ti,&cl,Cfile)!=1){
-	      printf("Cluster trajectory file to short, could not fill buffer!!!\n");
-	      printf("ITIME %d\n",ti);
-	      exit(1);
+        printf("Cluster trajectory file to short, could not fill buffer!!!\n");
+        printf("ITIME %d\n",ti);
+        exit(1);
       }
       /* Configuration belong to cluster */
       if (non->cluster==cl){
-	      Ncl++;
+        Ncl++;
       }
     }
 
@@ -113,22 +113,22 @@ void absorption(t_non *non){
       /* Loop over the three polarization directions */  
       for (x=0;x<3;x++){
         /* Read mu(ti) into auxillary array vecr and clear the imaginary part */
-        read_dipole(non,mu_traj,vecr,mu_xyz,x,ti);        
+        read_dipole(non,mu_traj,vecr,mu_xyz,x,ti);
         clearvec(veci,non->singles);
-
+        
         /* Loop over coherence time */
         for (t1=0;t1<non->tmax;t1++){
-	        tj=ti+t1;
-	        /* Read Hamiltonian */
+          tj=ti+t1;
+          
+          /* Read Hamiltonian */
           read_Hamiltonian(non,Hamil_i_e,H_traj,tj);
-	
-  	      /* Read mu(tj) */
+          /* Read mu(tj) */
           read_dipole(non,mu_traj,mu_eg,mu_xyz,x,tj);
-
-	        /* Do projection on selected sites if asked and calculate response function */
-	        if (non->Npsites==0){
-             /* Find response without projection */
-             calc_S1(re_S_1,im_S_1,t1,non,vecr,veci,mu_eg);
+          
+          /* Do projection on selected sites if asked and calculate response function */
+          if (non->Npsites==0){
+            /* Find response without projection */
+            calc_S1(re_S_1,im_S_1,t1,non,vecr,veci,mu_eg);
           } else if (non->Npsites<non->singles){
             projection(mu_eg,non);
             /* Find response with projection on single segment */
@@ -139,9 +139,9 @@ void absorption(t_non *non){
               multi_projection(mu_eg,mu_p,non,ip);
               calc_S1(re_S_1+non->tmax*ip,im_S_1+non->tmax*ip,t1,non,vecr,veci,mu_p);
             }
-	        }
-
-	        /* Probagate vector */
+          }
+          
+          /* Probagate vector */
           propagate_vector(non,Hamil_i_e,vecr,veci,1,samples,t1*x);
         }
       }
@@ -171,8 +171,8 @@ void absorption(t_non *non){
   /* Print information on number of realizations included belonging to the selected */
   /* cluster and close the cluster file. (Only to be done if cluster option is active.) */
   if (non->cluster!=-1){
-      printf("Of %d samples %d belonged to cluster %d.\n",samples,Ncl,non->cluster);
-      fclose(Cfile);
+    printf("Of %d samples %d belonged to cluster %d.\n",samples,Ncl,non->cluster);
+    fclose(Cfile);
   }
 
   /* Close Trajectory Files */
@@ -181,11 +181,11 @@ void absorption(t_non *non){
   /* Save time domain response */
   outone=fopen("TD_Absorption.dat","w");
   for (t1=0;t1<non->tmax1;t1+=non->dt1){
-     fprintf(outone,"%f ",t1*non->deltat);
-     for (ip=0;ip<pro_dim;ip++){
-        fprintf(outone,"%e %e ",re_S_1[t1+ip*non->tmax]/samples,im_S_1[t1+ip*non->tmax]/samples);
-     }
-     fprintf(outone,"\n");
+    fprintf(outone,"%f ",t1*non->deltat);
+    for (ip=0;ip<pro_dim;ip++){
+      fprintf(outone,"%e %e ",re_S_1[t1+ip*non->tmax]/samples,im_S_1[t1+ip*non->tmax]/samples);
+    }
+    fprintf(outone,"\n");
   }
   fclose(outone);
 
