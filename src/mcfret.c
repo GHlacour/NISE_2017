de05fe736a6acf582e7cd727af84273d7188ba5f
GHlacour <t.l.c.jansen@rug.nl>
2023-06-08 11:10:35 +0200

    Fixed bug in density matrix calculation for mcfret #1

--- src/mcfret.c
+++ src/mcfret.c
@@ -677,9 +677,8 @@ void density_matrix(float *density_matrix, float *Hamiltonian_i,t_non *non,int s
       }
   }
   /* Find eigenvalues and eigenvectors */
-  if (non->temperature<=100000){ /* Skip in high temperature approx */
-      diagonalizeLPD(H,e,N);
-  }
+  diagonalizeLPD(H,e,N);
+  
  
   /* Exponentiate [U=exp(-H/kBT)] */
   for (a=0;a<N;a++){
