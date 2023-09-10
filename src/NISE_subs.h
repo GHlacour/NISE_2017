df8f1bc248883769df1565404c4ee65ea14cd319
GHlacour <t.l.c.jansen@rug.nl>
2023-09-10 12:14:14 +0200

    Fixed a few compiler warnings and the error in the autodetection scheme when Hamiltonian contain a lot of integers

--- src/NISE_subs.h
+++ src/NISE_subs.h
@@ -27,14 +27,6 @@ void muread(t_non *non,float *leftnr,int ti,int x,FILE *mu_traj);
 void mureadE(t_non *non,float *leftnr,int ti,int x,FILE *mu_traj,float *mu,float *pol);
 int read_cluster(t_non *non,int pos,int *cl,FILE *FH);
 void propagate_vec_DIA(t_non *non,float *Hamiltonian_i,float *cr,float *ci,int sign);
-/*void propagate_t2_DIA(t_non *non,float *Hamiltonian_i,float *cr,float *ci,float **vr,float **vi,int sign);
-int propagate_vec_DIA_S(t_non *non,float *Hamiltonian_i,float *cr,float *ci,int sign);
-void propagate_vec_coupling_S(t_non *non,float *Hamiltonian_i,float *cr,float *ci,int m,int sign);
-void propagate_vec_coupling_S_doubles(t_non *non,float *Hamiltonian_i,float *cr,float
-*ci,int m,float *Anh);
-void propagate_vec_coupling_S_doubles_ES(t_non *non,float *Hamiltonian_i,float *cr,float *ci,int m);
-void diagonalizeLPD(float *H,float *v,int N);
-void build_diag_H(float *Hamiltonian_i,float *H,float *e,int N);*/
 void generateCS(float *X,float *Y,float *Z);
 //void projection(float *phi,t_non *non);
 int control(t_non *non);
@@ -44,9 +36,6 @@ void dipole_double_ground(t_non *non,float *dipole,float *fr,float *fi,float *ov
 void dipole_double_ES(t_non *non,float *dipole,float *cr,float *ci,float *fr,float *fi);
 void dipole_double_last(t_non *non,float *dipole,float *cr,float *ci,float *fr,float *fi,float *over);
 void dipole_double_last_ES(t_non *non,float *dipole,float *cr,float *ci,float *fr,float *fi);
-/*int time_evolution_mat(t_non *non,float *Hamiltonian_i,float *Ur,float *Ui,int *R,int *C,int m);
-void propagate_double_sparce(t_non *non,float *Ur,float *Ui,int *R,int *C,float *fr,float *fi,int elements,int m,float *Anh);
-void propagate_double_sparce_ES(t_non *non,float *Ur,float *Ui,int *R,int *C,float *fr,float *fi,int elements,int m);*/
 float distance(float *rf,float *ri,int a,int b,int N,float box);
 float distance_x(float *rf,float *ri,int a,int b,int N,float box,int x);
 float distance3(float *rf,float *ri,int a,int b,int N,float *box);
