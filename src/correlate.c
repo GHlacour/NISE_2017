0ca36fac7636fb365479adeed9661944eedb7981
GHlacour <t.l.c.jansen@rug.nl>
2023-09-20 12:00:48 +0200

    Added higher order correlation functions #26

--- src/correlate.c
+++ src/correlate.c
@@ -74,8 +74,11 @@ void calculateCorrelation(float *input1, float *input2, float *output, int N) {
 /* Control calculation of correlation functions */
 void calc_Correlation(t_non *non){
     float *corr_matrix;
+    float *c3_matrix;
+    float *c4_matrix;
     float *traj1;
     float *traj2;
+    float *traj3;
     float *corr;
     float *Hamil_i_e;
     int TT,T,N,nn2;
@@ -94,8 +97,11 @@ void calc_Correlation(t_non *non){
     nn2=N*(N+1)/2;
     traj1=(float *)calloc(TT,sizeof(float));
     traj2=(float *)calloc(TT,sizeof(float));
+    traj3=(float *)calloc(TT,sizeof(float));
     corr=(float *)calloc(TT,sizeof(float));
     corr_matrix=(float *)calloc(nn2*T,sizeof(float));
+    c3_matrix=(float *)calloc(N*T,sizeof(float));
+    c4_matrix=(float *)calloc(N*T,sizeof(float));
     Hamil_i_e=(float *)calloc(nn2,sizeof(float));
 
     /* Open Trajectory files */
@@ -119,6 +125,25 @@ void calc_Correlation(t_non *non){
 		//printf("%f ",traj1[ti]);
 		corr_matrix[Sindex(a,b,N)*T+ti]=corr[ti]/TT;
 	    }
+	    /* Higher order correlations */
+	    if (a==b){
+                for (ti=0;ti<TT;ti++){
+		    traj3[ti]=traj1[ti]*traj1[ti];
+		}
+		subtractMean(traj3,TT);
+		/* Third order */
+		calculateCorrelation(traj3,traj1,corr,TT);
+		/* Store in matrix */
+                for (ti=0;ti<T;ti++){
+                    c3_matrix[a*T+ti]=corr[ti]/TT;
+		}
+		/* Fourth order */
+		calculateCorrelation(traj3,traj3,corr,TT);
+                /* Store in matrix */
+                for (ti=0;ti<T;ti++){
+                    c4_matrix[a*T+ti]=corr[ti]/TT;
+                }
+            }
 	}
     }
 
@@ -134,9 +159,31 @@ void calc_Correlation(t_non *non){
     }
     fclose(outone);
 
+    outone=fopen("SkewnessMatrix.dat","w");
+    for (ti=0;ti<T;ti++){
+        fprintf(outone,"%f ",ti*non->deltat);
+        /* Loop through pairs */
+        for (a=0;a<N;a++){
+            fprintf(outone,"%e ",c3_matrix[a*T+ti]);
+        }
+        fprintf(outone,"\n");
+    }
+    fclose(outone);
+
+    outone=fopen("KurtosisMatrix.dat","w");
+    for (ti=0;ti<T;ti++){
+        fprintf(outone,"%f ",ti*non->deltat);
+        /* Loop through pairs */
+        for (a=0;a<N;a++){
+            fprintf(outone,"%e ",c4_matrix[a*T+ti]);
+        }
+        fprintf(outone,"\n");
+    }
+    fclose(outone);
 
     /* Close Trajectory Files */
     fclose(mu_traj),fclose(H_traj);
-    free(traj1),free(traj2),free(corr),free(corr_matrix);
+    free(traj1),free(traj2),free(traj3),free(corr),free(corr_matrix);
+    free(c3_matrix),free(c4_matrix);
     free(Hamil_i_e);
 }
