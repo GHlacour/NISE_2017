6d9d2c52d8411e661b1be794774f776cd5300cfc
Thomas la Cour Jansen <t.l.c.jansen@rug.nl>
2024-10-14 13:12:09 +0200

    Spectral Density
    The spectral density is now stored to file, however, it has a very finegrained frequency resolution, which we may want to smear out. The unit should also be validated #82

--- src/correlate.c
+++ src/correlate.c
@@ -32,7 +32,7 @@ void subtractMean(float* signal, int N) {
 
 
 // Function to calculate correlation function using FFTW
-void calculateCorrelation(float *input1, float *input2, float *output, int N) {
+void calculateCorrelation(float *input1, float *input2, float *output, float *SD, int N) {
     // Create FFTW plans
     fftwf_plan plan1, plan2, plan3;
 
@@ -56,9 +56,11 @@ void calculateCorrelation(float *input1, float *input2, float *output, int N) {
     }
 
     // Multiply the two complex spectra element-wise
+    // And store real part in Spectral Density
     for (int i = 0; i < N; i++) {
         fft_output[i][0] = (fft_input1[i][0] * fft_input2[i][0]) - (fft_input1[i][1] * fft_input2[i][1]);
         fft_output[i][1] = (fft_input1[i][0] * fft_input2[i][1]) + (fft_input1[i][1] * fft_input2[i][0]);
+        SD[i]=fft_output[i][0]/N;
     }
 
     // Execute inverse FFT to get the correlation function
@@ -89,6 +91,7 @@ void calc_Correlation(t_non *non){
     float *traj3;
     float *corr;
     float *Hamil_i_e;
+    float *SD,*SD_matrix;
     int TT,T,N,nn2;
     int a,b; /* Indices for chromophores */
     int ti;
@@ -97,6 +100,7 @@ void calc_Correlation(t_non *non){
     FILE *H_traj,*mu_traj;
     FILE *C_traj;
     FILE *outone,*log;
+    FILE *outSD;
     FILE *Cfile;
 
     TT=non->length; /* Total timesteps */
@@ -107,7 +111,9 @@ void calc_Correlation(t_non *non){
     traj2=(float *)calloc(TT,sizeof(float));
     traj3=(float *)calloc(TT,sizeof(float));
     corr=(float *)calloc(TT,sizeof(float));
+    SD=(float *)calloc(TT,sizeof(float));
     corr_matrix=(float *)calloc(nn2*T,sizeof(float));
+    SD_matrix=(float *)calloc(N*TT,sizeof(float));
     c3_matrix=(float *)calloc(N*T,sizeof(float));
     c4_matrix=(float *)calloc(N*T,sizeof(float));
     Hamil_i_e=(float *)calloc(nn2,sizeof(float));
@@ -117,65 +123,82 @@ void calc_Correlation(t_non *non){
 
     /* Loop over pairs */
     for (a=0;a<N;a++){
-	for (b=a;b<N;b++){
-            for (ti=0;ti<TT;ti++){
-                /* Read Hamiltonian */
-		read_Hamiltonian(non,Hamil_i_e,H_traj,ti);
-		traj1[ti]=Hamil_i_e[Sindex(a,a,N)];
-		traj2[ti]=Hamil_i_e[Sindex(b,b,N)];
+	  for (b=a;b<N;b++){
+        for (ti=0;ti<TT;ti++){
+          /* Read Hamiltonian */
+		  read_Hamiltonian(non,Hamil_i_e,H_traj,ti);
+		  traj1[ti]=Hamil_i_e[Sindex(a,a,N)];
+		  traj2[ti]=Hamil_i_e[Sindex(b,b,N)];
 	    }
 	    /* Do WK theorm */
 	    subtractMean(traj1,TT);
 	    subtractMean(traj2,TT);
-            calculateCorrelation(traj1,traj2,corr,TT);
+        calculateCorrelation(traj1,traj2,corr,SD,TT);
 	    /* Store in matrix */
 	    for (ti=0;ti<T;ti++){
 		//printf("%f ",traj1[ti]);
-		corr_matrix[Sindex(a,b,N)*T+ti]=corr[ti]/TT;
-	    }
+		  corr_matrix[Sindex(a,b,N)*T+ti]=corr[ti]/TT;
+        }
+        if (a==b) {
+          for (ti=0;ti<TT;ti++){
+            SD_matrix[a*TT+ti]=SD[ti];
+	      }
+        }
 	    /* Higher order correlations */
 	    if (a==b){
-                for (ti=0;ti<TT;ti++){
+          for (ti=0;ti<TT;ti++){
 		    traj3[ti]=traj1[ti]*traj1[ti];
-		}
-		subtractMean(traj3,TT);
-		/* Third order */
-		calculateCorrelation(traj3,traj1,corr,TT);
-		/* Store in matrix */
-                for (ti=0;ti<T;ti++){
-                    c3_matrix[a*T+ti]=corr[ti]/TT;
-		}
-		/* Fourth order */
-		calculateCorrelation(traj3,traj3,corr,TT);
-                /* Store in matrix */
-                for (ti=0;ti<T;ti++){
-                    c4_matrix[a*T+ti]=corr[ti]/TT;
-                }
-            }
+		  }
+		  subtractMean(traj3,TT);
+		  /* Third order */
+		  calculateCorrelation(traj3,traj1,corr,SD,TT);
+		  /* Store in matrix */
+          for (ti=0;ti<T;ti++){
+            c3_matrix[a*T+ti]=corr[ti]/TT;
+		  }
+		  /* Fourth order */
+		  calculateCorrelation(traj3,traj3,corr,SD,TT);
+          /* Store in matrix */
+          for (ti=0;ti<T;ti++){
+            c4_matrix[a*T+ti]=corr[ti]/TT;
+          }
+        }
 	    /* Skip cross correlations if in autocorrelate mode */
 	    if (string_in_array(non->technique,
-                                    (char*[]){"Autocorrelation"},1)){
-                break;
+          (char*[]){"Autocorrelation"},1)){
+          break;
 	    }
-	}
+	  }
     }
 
     /* Save to file */
     outone=fopen("CorrelationMatrix.dat","w");
     for (ti=0;ti<T;ti++){
-        fprintf(outone,"%f ",ti*non->deltat);
-	/* Loop through pairs */
-	for (a=0;a<N;a++){
-            for (b=a;b<N;b++){
-                fprintf(outone,"%e ",corr_matrix[Sindex(a,b,N)*T+ti]);
-		/* Skip cross correlations if in autocorrelate mode */
-                if (string_in_array(non->technique,
-                                    (char*[]){"Autocorrelation"},1)){
-                    break;
-                }
+      fprintf(outone,"%f ",ti*non->deltat);
+	  /* Loop through pairs */
+	  for (a=0;a<N;a++){
+        for (b=a;b<N;b++){
+          fprintf(outone,"%e ",corr_matrix[Sindex(a,b,N)*T+ti]);
+		  /* Skip cross correlations if in autocorrelate mode */
+          if (string_in_array(non->technique,
+            (char*[]){"Autocorrelation"},1)){
+            break;
+          }
 	    }
-	}
-        fprintf(outone,"\n");
+	  }
+      fprintf(outone,"\n");
+    }
+    fclose(outone);
+
+    /* Save Spectral Density to file */
+    outone=fopen("SpectralDensity.dat","w");
+    for (ti=0;ti<TT;ti++){
+      fprintf(outone,"%f ",ti/non->deltat/icm2ifs/twoPi/TT);
+	  /* Loop through pairs */
+	  for (a=0;a<N;a++){
+        fprintf(outone,"%e ",SD_matrix[a*TT+ti]);
+      }
+      fprintf(outone,"\n");
     }
     fclose(outone);
 
@@ -204,6 +227,8 @@ void calc_Correlation(t_non *non){
     /* Close Trajectory Files */
     fclose(mu_traj),fclose(H_traj);
     free(traj1),free(traj2),free(traj3),free(corr),free(corr_matrix);
+    free(SD_matrix);
+    free(SD);
     free(c3_matrix),free(c4_matrix);
     free(Hamil_i_e);
 }
--- src/correlate.h
+++ src/correlate.h
@@ -1,4 +1,7 @@
+#ifndef _CORRELATE_
+#define _CORRELATE_
 void subtractMean(float* signal, int N);
-void calculateCorrelation(float *input1, float *input2, float *output, int N);
+void calculateCorrelation(float *input1, float *input2, float *output,float *SD, int N);
 
 void calc_Correlation(t_non *non);
+#endif
